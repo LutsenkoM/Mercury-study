f93bd54c9c348f29d6d4c9ce13d0d34ab2563547
Alina Lutsenko <alinaternova@gmail.com>
2018-06-21 22:42:38 +0300

    Refactoring

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 class App extends React.Component {
   render() {
     return (
-        <section className="merkury-wrap">
+        <section className="mercury-wrap">
             <div className="admin-block">
                 <Header/>
                 <Main/>
